597b5783d77603bb88fef5630e53d6226d1c81df
Wiktor Keska <wkeska@silenteight.com>
2021-07-08 10:46:16 +0200

    moved use cases into grpc services

--- src/main/java/com/silenteight/aedemo/datasource/CategoryGrpcService.java
+++ src/main/java/com/silenteight/aedemo/datasource/CategoryGrpcService.java
@@ -6,21 +6,17 @@ import com.silenteight.datasource.categories.api.v1.CategoryServiceGrpc.Category
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @GrpcService
 class CategoryGrpcService extends CategoryServiceImplBase {
-  private final ListCategoriesUseCase categoriesUseCase;
-  private final GetMatchCategoryValuesUseCase categoryValuesUseCase;
-
-  CategoryGrpcService(ListCategoriesUseCase categoriesUseCase, GetMatchCategoryValuesUseCase categoryValuesUseCase) {
-    this.categoriesUseCase = categoriesUseCase;
-    this.categoryValuesUseCase = categoryValuesUseCase;
-  }
 
   @Override
   public void listCategories(
       ListCategoriesRequest request,
       StreamObserver<ListCategoriesResponse> responseObserver) {
-    var categories = categoriesUseCase.findAllCategories();
+    var categories = findAllCategories();
     responseObserver.onNext(
         ListCategoriesResponse.newBuilder().addAllCategories(categories).build());
     responseObserver.onCompleted();
@@ -30,7 +26,7 @@ class CategoryGrpcService extends CategoryServiceImplBase {
   public void batchGetMatchCategoryValues(
       BatchGetMatchCategoryValuesRequest request,
       StreamObserver<BatchGetMatchCategoryValuesResponse> responseObserver) {
-    var categoryValues = categoryValuesUseCase.getMatchCategoryValues(
+    var categoryValues = getMatchCategoryValues(
         request.getMatchValuesList());
     responseObserver.onNext(
         BatchGetMatchCategoryValuesResponse
@@ -38,5 +34,55 @@ class CategoryGrpcService extends CategoryServiceImplBase {
             .addAllCategoryValues(categoryValues)
             .build());
     responseObserver.onCompleted();
+  }
+
+  private List<Category> findAllCategories() {
+    return List.of(
+        Category.newBuilder().setName("country")
+            .setType(CategoryType.ANY_STRING)
+            .setDisplayName("Country")
+            .setMultiValue(true)
+            .build(),
+        Category.newBuilder().setName("customerType")
+            .setDisplayName("Customer Type (Individual / Company)")
+            .setType(CategoryType.ENUMERATED)
+            .setMultiValue(false)
+            .addAllAllowedValues(List.of("I", "C"))
+            .build(),
+        Category.newBuilder().setName("hitType")
+            .setDisplayName("Hit Type (PEP / AM / SAN)")
+            .setType(CategoryType.ENUMERATED)
+            .setMultiValue(false)
+            .addAllAllowedValues(List.of("SAN", "PEP", "AM"))
+            .build());
+  }
+
+  private List<CategoryValue> getMatchCategoryValues(List<String> matchValues) {
+    return matchValues.stream()
+        .map(this::getCategoryValue)
+        .collect(Collectors.toList());
+  }
+
+  private CategoryValue getCategoryValue(String matchValue) {
+    var builder = CategoryValue.newBuilder()
+        .setName(matchValue);
+
+    if (matchValue.contains("categories/source_system")) {
+      builder.setSingleValue("ECDD");
+    } else if (matchValue.contains("categories/country")) {
+      builder.setSingleValue("PL");
+    } else if (matchValue.contains("categories/customer_type")) {
+      builder.setSingleValue("I");
+    } else if (matchValue.contains("categories/hit_type")) {
+      builder.setSingleValue("DENY");
+    } else if (matchValue.contains("categories/segment")) {
+      builder.setSingleValue("CONSUMER");
+    } else if (matchValue.contains("categories/hit_category")) {
+      builder.setSingleValue("DENY");
+    } else {
+      builder.setSingleValue("UNKNOWN");
+    }
+
+    return builder.build();
   }
 }
--- src/main/java/com/silenteight/aedemo/datasource/CommentInputGrpcService.java
+++ src/main/java/com/silenteight/aedemo/datasource/CommentInputGrpcService.java
@@ -4,28 +4,38 @@ import com.silenteight.datasource.comments.api.v1.CommentInput;
 import com.silenteight.datasource.comments.api.v1.CommentInputServiceGrpc.CommentInputServiceImplBase;
 import com.silenteight.datasource.comments.api.v1.StreamCommentInputsRequest;
 
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.context.annotation.Profile;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @GrpcService
 class CommentInputGrpcService extends CommentInputServiceImplBase {
-
-  private final CommentsInputUseCase commentsInputUseCase;
-
-  CommentInputGrpcService(CommentsInputUseCase commentsInputUseCase) {
-    this.commentsInputUseCase = commentsInputUseCase;
-  }
 
   @Override
   public void streamCommentInputs(
       StreamCommentInputsRequest request,
       StreamObserver<CommentInput> responseObserver) {
 
-    var commentInputs = commentsInputUseCase
-        .getCommentInputs(request.getAlertsList());
+    var commentInputs = getCommentInputs(request.getAlertsList());
     commentInputs.stream().forEach(responseObserver::onNext);
 
     responseObserver.onCompleted();
+  }
+
+  private List<CommentInput> getCommentInputs(List<String> alerts) {
+    return alerts.stream().map(alert ->
+        CommentInput.newBuilder()
+            .setAlert(alert)
+            .setAlertCommentInput(Struct.newBuilder()
+                .putFields("field1", Value.newBuilder().setStringValue("test1-" + alert).build())
+                .putFields("field2", Value.newBuilder().setNumberValue(2).build())
+                .build())
+            .build()
+    ).collect(Collectors.toList());
   }
 }
